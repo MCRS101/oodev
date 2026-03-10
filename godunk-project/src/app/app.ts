5ed0f9c2165564249337e2ff6d2c36a8a9aef073
Witt5D <sawit.sr67@snru.ac.th>
2026-03-11 01:46:33 +0700

    update1

--- godunk-project/src/app/app.ts
+++ godunk-project/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./navbar/navbar";
+import { Prodcrd } from './prodcrd/prodcrd';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar,Prodcrd],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ godunk-project/src/app/prodcrd/prodcrd.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodcrd',
+  imports: [],
+  templateUrl: './prodcrd.html',
+  styleUrl: './prodcrd.css',
+})
+export class Prodcrd {
+
+}
