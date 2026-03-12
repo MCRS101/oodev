a31d92a2a54e84e1241500f35af3e687de147471
Mercury <Copter0001.yahoo.com@gmail.com>
2026-03-12 21:25:24 +0700

    home

--- godunk-sell/src/app/login/login.ts
+++ godunk-sell/src/app/login/login.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
+ email = "";
+  password = "";
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  login(){
+
+    const body = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.http.post("http://localhost:3000/api/login", body)
+    .subscribe({
+      next:(res:any)=>{
+
+        alert("Login success");
+
+        console.log(res);
+
+        // บันทึก user
+        localStorage.setItem("user", JSON.stringify(res.user));
+
+        // ไปหน้า dashboard
+        this.router.navigate(['/home']);
+
+      },
+      error:(err)=>{
+        console.log(err);
+        alert("Email หรือ Password ไม่ถูกต้อง");
+      }
+    });
+
+  }
 
 }
